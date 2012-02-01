9c6f7ac7731053c4ffdae2edf8c34b84687950d8
Martin Kovarik <kovarik.martin@gmail.com>
2012-02-01 21:11:10 +0100

    node listening port update to be compatible with no.de hosting

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.get('/stocks/:tickers', function(req, res) {
   request(url).pipe(res);
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
