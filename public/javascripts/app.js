1c441258c00b8eba50df0fda5f03d9a3afb6cc6f
Martin Kovarik <kovarik.martin@gmail.com>
2012-02-02 15:23:15 +0100

    parsing actual stock data and displaying in page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,19 @@ App.constants = {
 };
 
 App.Stock = Em.Object.extend({
-	ticker: ''	
+	ticker: '',
+  property1: '',
+  property2: '',
+  property3: '',
+  property4: '',
+  property5: '',
+  property6: '',
+  property7: '',
+  property8: '',
+  property9: '',
+  property10: '',
+  property11: '',
+  property12: ''
 });
 
 App.stocks = [
@@ -36,10 +48,31 @@ App.stocksController = Em.ArrayProxy.create({
 		return url;
 	},
 
-    parseCsv: function(data) {
-        console.log("CSV data: ", data);
-		var csvObject = CSV.csvToArray(data);
-		console.log("CSV object: ", csvObject);
-		return csvObject;
-	}
+  parseCsv: function(data) {
+    console.log("CSV data: ", data);
+    var csvObject = CSV.csvToArray(data);
+    console.log("CSV object: ", csvObject);
+
+    var stocks = csvObject.map(function (item, index, self) {
+      return App.Stock.create({
+        ticker: item[0],
+        property1: item[1],
+        property2: item[2],
+        property3: item[3],
+        property4: item[4],
+        property5: item[5],
+        property6: item[6],
+        property7: item[7],
+        property8: item[8],
+        property9: item[9],
+        property10: item[10],
+        property11: item[11],
+        property12: item[12]
+      });
+    });
+
+    App.stocksController.content.replace(0, App.stocksController.content.length, stocks);
+
+    return csvObject;
+  }
 });
